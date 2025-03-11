bfea50b840810e7422ab8136e69fae8f70f2c4cf
sarapeddy <269362@studenti.unimore.it>
2025-03-11 12:18:46 +0100

    Add the command to run the model on the other datasets

--- SimTS/forecasting.sh
+++ SimTS/forecasting.sh
@@ -1,17 +1,25 @@
 
 # To run SimTS model with pipeline integration on forecastin task
 python3 script.py --dataset ETTh1 --eval
+python3 script.py --dataset ETTh2 --eval
 python3 script.py --dataset ETTm1 --eval
+python3 script.py --dataset ETTm2 --eval
+python3 script.py --dataset exchange_rate --eval
 python3 script.py --dataset WTH --eval
 python3 script.py --dataset electricity --eval
+python3 script.py --dataset weather --eval
 python3 script.py --dataset traffic --eval
 python3 script.py --dataset national_illness --eval
 
 # To run original SimTS model on forecasting task
 python3 train_simts.py --dataset ETTh1 --eval
+python3 train_simts.py --dataset ETTh2 --eval
 python3 train_simts.py --dataset ETTm1 --eval
+python3 train_simts.py --dataset ETTm2 --eval
+python3 train_simts.py --dataset exchange_rate --eval
 python3 train_simts.py --dataset WTH --eval
 python3 train_simts.py --dataset electricity --eval
+python3 train_simts.py --dataset weather --eval
 python3 train_simts.py --dataset traffic --eval
 python3 train_simts.py --dataset national_illness --eval
 
