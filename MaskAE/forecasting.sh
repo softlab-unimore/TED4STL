7d050bed2f3d8677ba6b92b9b058ff4e96d58412
sarapeddy <269362@studenti.unimore.it>
2025-03-11 11:40:18 +0100

    Add command for all the datasets

--- MaskAE/forecasting.sh
+++ MaskAE/forecasting.sh
@@ -2,36 +2,60 @@
 python3 script_forecasting_dlinear.py --dataset ETTh1 --mode dlinear
 python3 finetune_script_forecasting_dlinear.py --dataset ETTh1 --mode dlinear
 
+python3 script_forecasting_dlinear.py --dataset ETTh1 --mode dlinear
+python3 finetune_script_forecasting_dlinear.py --dataset ETTh1 --mode dlinear
+
 python3 script_forecasting_dlinear.py --dataset ETTm1 --mode dlinear
 python3 finetune_script_forecasting_dlinear.py --dataset ETTm1 --mode dlinear
 
-python3 script_forecasting_dlinear.py --dataset  national_illness --mode dlinear --n_length 96 --batch_size 32
-python3 finetune_script_forecasting_dlinear.py --dataset national_illness --mode dlinear --n_length 96 --finetune_batch_size 32 --batch_size 32
+python3 script_forecasting_dlinear.py --dataset ETTm2 --mode dlinear
+python3 finetune_script_forecasting_dlinear.py --dataset ETTm2 --mode dlinear
+
+python3 script_forecasting_dlinear.py --dataset exchange_rate --mode dlinear
+python3 finetune_script_forecasting_dlinear.py --dataset exchange_rate --mode dlinear
 
 python3 script_forecasting_dlinear.py --dataset  WTH --mode dlinear
 python3 finetune_script_forecasting_dlinear.py --dataset WTH --mode dlinear
 
+python3 script_forecasting_dlinear.py --dataset electricity --mode dlinear
+python3 finetune_script_forecasting_dlinear.py --dataset electricity --mode dlinear
+
+python3 script_forecasting_dlinear.py --dataset weather --mode dlinear
+python3 finetune_script_forecasting_dlinear.py --dataset weather --mode dlinear
+
 python3 script_forecasting_dlinear.py --dataset traffic --mode dlinear
 python3 finetune_script_forecasting_dlinear.py --dataset traffic --mode dlinear
 
-python3 script_forecasting_dlinear.py --dataset electricity --mode dlinear
-python3 finetune_script_forecasting_dlinear.py --dataset electricity --mode dlinear
+python3 script_forecasting_dlinear.py --dataset  national_illness --mode dlinear --n_length 96 --batch_size 32
+python3 finetune_script_forecasting_dlinear.py --dataset national_illness --mode dlinear --n_length 96 --finetune_batch_size 32 --batch_size 32
 
 # To run the experiments on MAE without the pipeline integration on forecasting task
 python3 script_forecasting_normal.py --dataset ETTh1 --mode MAE
 python3 finetune_script_forecasting_normal.py --dataset ETTh1 --mode MAE
 
+python3 script_forecasting_normal.py --dataset ETTh2 --mode MAE
+python3 finetune_script_forecasting_normal.py --dataset ETTh2 --mode MAE
+
 python3 script_forecasting_normal.py --dataset ETTm1 --mode MAE
 python3 finetune_script_forecasting_normal.py --dataset ETTm1 --mode MAE
 
-python3 script_forecasting_normal.py --dataset  national_illness --mode MAE --n_length 96 --batch_size 32
-python3 finetune_script_forecasting_normal.py --dataset national_illness --mode MAE --n_length 96 --finetune_batch_size 32 --batch_size 32
+python3 script_forecasting_normal.py --dataset ETTm2 --mode MAE
+python3 finetune_script_forecasting_normal.py --dataset ETTm2 --mode MAE
+
+python3 script_forecasting_normal.py --dataset exchange_rate --mode MAE
+python3 finetune_script_forecasting_normal.py --dataset exchange_rate --mode MAE
 
 python3 script_forecasting_normal.py --dataset  WTH --mode MAE
 python3 finetune_script_forecasting_normal.py --dataset WTH --mode MAE
 
+python3 script_forecasting_normal.py --dataset electricity --mode MAE
+python3 finetune_script_forecasting_normal.py --dataset electricity --mode MAE
+
+python3 script_forecasting_normal.py --dataset weather --mode MAE
+python3 finetune_script_forecasting_normal.py --dataset weather --mode MAE
+
 python3 script_forecasting_normal.py --dataset traffic --mode MAE
 python3 finetune_script_forecasting_normal.py --dataset traffic --mode MAE
 
-python3 script_forecasting_normal.py --dataset electricity --mode MAE
-python3 finetune_script_forecasting_normal.py --dataset electricity --mode MAE
+python3 script_forecasting_normal.py --dataset  national_illness --mode MAE --n_length 96 --batch_size 32
+python3 finetune_script_forecasting_normal.py --dataset national_illness --mode MAE --n_length 96 --finetune_batch_size 32 --batch_size 32
