c9a3f6a67918d53feb30425a490b6df88162a523
sarapeddy <269362@studenti.unimore.it>
2025-03-11 13:02:00 +0100

    Add command for all the datasets

--- Ts2Vec/forecasting.sh
+++ Ts2Vec/forecasting.sh
@@ -1,20 +1,28 @@
 
 # To execute the Ts2Vec model with the pipeline integration on forecasting tasks
 python3 script_forecasting.py --mode ts2vec-one-loss --dataset ETTh1 --eval
+python3 script_forecasting.py --mode ts2vec-one-loss --dataset ETTh2 --eval
 python3 script_forecasting.py --mode ts2vec-one-loss --dataset ETTm1 --eval
+python3 script_forecasting.py --mode ts2vec-one-loss --dataset ETTm2 --eval
+python3 script_forecasting.py --mode ts2vec-one-loss --dataset exchange_rate --eval
 pyhton3 script_forecasting.py --mode ts2vec-one-loss --dataset WTH --eval
 pyhton3 script_forecasting.py --mode ts2vec-one-loss --dataset electricity --eval
+pyhton3 script_forecasting.py --mode ts2vec-one-loss --dataset weather --eval
 pyhton3 script_forecasting.py --mode ts2vec-one-loss --dataset traffic --eval
 pyhton3 script_forecasting.py --mode ts2vec-one-loss --dataset national_illness --eval
 
 
 # To execute the Ts2Vec original model
-python3 script_forecasting.py --mode ts2vec --dataset ETTh1
-python3 script_forecasting.py --mode ts2vec --dataset ETTm1
-pyhton3 script_forecasting.py --mode ts2vec --dataset WTH
-pyhton3 script_forecasting.py --mode ts2vec --dataset electricity
-pyhton3 script_forecasting.py --mode ts2vec --dataset traffic
-pyhton3 script_forecasting.py --mode ts2vec --dataset national_illness
+python3 script_forecasting.py --mode ts2vec --dataset ETTh1 --eval
+python3 script_forecasting.py --mode ts2vec --dataset ETTh2 --eval
+python3 script_forecasting.py --mode ts2vec --dataset ETTm1 --eval
+python3 script_forecasting.py --mode ts2vec --dataset ETTm2 --eval
+python3 script_forecasting.py --mode ts2vec --dataset exchange_rate --eval
+pyhton3 script_forecasting.py --mode ts2vec --dataset WTH --eval
+pyhton3 script_forecasting.py --mode ts2vec --dataset electricity --eval
+pyhton3 script_forecasting.py --mode ts2vec --dataset weather --eval
+pyhton3 script_forecasting.py --mode ts2vec --dataset traffic --eval
+pyhton3 script_forecasting.py --mode ts2vec --dataset national_illness --eval
 
 
 
